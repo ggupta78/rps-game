69f5fe2cb57e963d6dc4d025042069f3c00cecdf
Gautam Gupta <ggupta78@gmail.com>
2023-02-12 13:49:05 +0530

    complete functionality

--- rps.js
+++ rps.js
@@ -1,5 +1,50 @@
+const allInputs = document.querySelectorAll("input");
+const message = document.querySelector(".message");
+const score = document.querySelector(".score");
+const rounds = document.querySelector(".rounds");
+
+let currentRound = 1, maxWins = 3;
+let playerWinCount = 0, computerWinCount = 0;
+let gameOver = false;
+
+function handleBtnClick() {
+  if (gameOver) {
+    currentRound = 1;
+    playerWinCount = computerWinCount = 0;
+    gameOver = false;
+  }
+
+  console.log(`${this.name}`);
+  const playerSelection = this.name;
+  const computerSelection = getComputerChoice();
+  const result = playRound(playerSelection, computerSelection);
+  score.innerHTML = `You: ${playerWinCount}, Computer: ${computerWinCount}`;
+
+  currentRound++;
+
+  if (playerWinCount === maxWins || computerWinCount === maxWins) {
+    gameOver = true;
+    let finalWinner = "";
+    if (playerWinCount === maxWins) {
+      finalWinner = `You won ${maxWins} rounds! You are the Champion!!`;
+    } else {
+      finalWinner = `Computer won ${maxWins} rounds! Computer is the Champion!!`;
+    }
+    message.innerHTML = finalWinner;
+  } else {
+    message.innerHTML = result;
+  }
+
+  rounds.innerHTML = `Round ${currentRound}`;
+};
+
+allInputs.forEach(input => {
+  input.addEventListener('click', handleBtnClick);
+});
+
+
 function getComputerChoice() {
-  const choices = ['rock', 'paper', 'scissors'];
+  const choices = ['Rock', 'Paper', 'Scissors'];
   const randomChoice = Math.floor(Math.random() * 3);
 
   return choices[randomChoice];
@@ -9,20 +54,23 @@ function playRound(playerSelection, computerSelection) {
   let playerWins = false;
 
   if (playerSelection === computerSelection) {
+
     return "It's a Tie! Try again!";
   }
 
-  if (playerSelection === 'rock' && computerSelection === 'scissors') {
+  if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
     playerWins = true;
-  } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+  } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
     playerWins = true;
-  } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+  } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
     playerWins = true;
   }
 
   if (playerWins) {
+    playerWinCount++;
     return `You Win! ${playerSelection} beats ${computerSelection}`;
   } else {
+    computerWinCount++;
     return `You Lose! ${computerSelection} beats ${playerSelection}`;
   }
 }
